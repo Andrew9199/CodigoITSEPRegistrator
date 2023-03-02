b3e0ac5fdd593cc055a6bd07102babe700617349
Andrew Williams <alexissanchezd97@gmail.com>
2023-03-02 12:55:29 -0500

    implementacion de clases en Java a la subclase, profesor usando el metodo Person

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    
+    public static void main(String[] args){
+        Profesor Profesor1 = new Profesor("Alexis Sanchez", 21, "Ingles", "Mañana");
+   System.out.println("El nombre del profesor es " + Profesor1.nombre + ", tiene " + Profesor1.edad + " años imparte la materia de " + Profesor1.materia + " y su turno es por " + Profesor1.turno);
+    }
+}
