642d0a1e251f028baee839e456ff99d8b6bb44d5
Andrew Williams <alexissanchezd97@gmail.com>
2023-03-02 11:58:11 -0500

    El primer commit del proyecto de ITSEP, construccion del codigo SQL para registrar profesores, registrar entrada y salida y un ejemplo de Querry para ingresar la hora

---
+++ EntradaProfesores.sql
@@ -0,0 +1,7 @@
+CREATE TABLE EntradaProfesores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    profesor_id INT,
+    fecha_hora_entrada DATETIME,
+    fecha_hora_salida DATETIME,
+    FOREIGN KEY (profesor_id) REFERENCES Profesores(id)
+);
